e7f8cb49239b6364d3045ad49cb494f92b60c2a8
tulgarozdilek <t.ozdilek@gmail.com>
2024-09-02 18:18:13 +0300

    await added

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -6,14 +6,14 @@ import { beforeEach, describe } from 'node:test';
 describe('New Todo', async () => {
   test.beforeEach('navigate tot todo app', async ({ page }) => {
     await page.goto('');
-  });
+  })
 
   test('active and completed filters', async ({ page }) => {
-    const todoinput =  page.getByPlaceholder('What needs to be done?')
-    todoinput.fill('water the plants');
-    todoinput.press('Enter');
-    todoinput.fill('feed the dog');
-    todoinput.press('Enter');
+    const todoInput = page.getByPlaceholder('What needs to be done?')
+    await todoInput.fill('water the plants');
+    await todoInput.press('Enter');
+    await todoInput.fill('feed the dog');
+    await todoInput.press('Enter');
     await page.locator('li').filter({ hasText: 'water the plants' }).getByLabel('Toggle Todo').check();
     await page.getByRole('link', { name: 'Active' }).click();
     await expect(page.getByTestId('todo-title')).toContainText('feed the dog');
@@ -22,10 +22,9 @@ describe('New Todo', async () => {
   });
 
   test('text field is cleared when item is adedd', async ({ page }) => {
-    const todoinput =  page.getByPlaceholder('What needs to be done?')
-    todoinput.fill("water the plants");
-    todoinput.press('Enter');
+    const todoInput = page.getByPlaceholder('What needs to be done?')
+    await todoInput.fill("water the plants");
+    await todoInput.press('Enter');
     await expect(page.getByPlaceholder('What needs to be done?')).toBeEmpty();
-  });
-});
-
+  })
+})
